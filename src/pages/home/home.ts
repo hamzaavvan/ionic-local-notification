b5dd07607544540611b27e097bd715d0d6d840fc
Hamza AVvan <hamzaavvan@gmail.com>
2017-12-25 21:24:31 +0500

    a commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,42 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform, AlertController } from 'ionic-angular';
+import { LocalNotifications } from '@ionic-native/local-notifications';
+import * as moment from 'moment';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  notifyTime: any;
+  notifications: any[] = [];
+  days: any[];
+  chosenHours: number;
+  chosenMinutes: number;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public platform: Platform, public alertCtrl: AlertController, public localNotifications: LocalNotifications) {
+    this.notifyTime = moment(new Date()).format();
 
+    this.chosenHours = new Date().getHours();
+    this.chosenMinutes = new Date().getMinutes();
+
+    this.days = [
+      {title: "Monday", dayCode: 1, checked: false},
+      {title: "Tuesday", dayCode: 1, checked: false},
+      {title: 'Wednesday', dayCode: 3, checked: false},
+      {title: 'Thursday', dayCode: 4, checked: false},
+      {title: 'Friday', dayCode: 5, checked: false},
+      {title: 'Saturday', dayCode: 6, checked: false},
+      {title: 'Sunday', dayCode: 0, checked: false}
+    ];
   }
+
+  ionViewDidLoad() {}
+
+  timeChange(time) {}
+
+  addNotifications() {}
+
+  cancellAll() {}
 
 }
